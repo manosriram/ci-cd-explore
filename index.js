5f3c4f147422e156aaac7a93e1663cebb6575eb6
Mano Sriram <mano.sriram0@gmail.com>
2020-12-19 14:01:35 +0530

    route message update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
     return res.send({
-        message: "Update with pm2 running."
+        message: "Update with pm2 running and workflow script change."
     });
 });
 

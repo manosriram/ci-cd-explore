e4f1e7947412ad342a2bba1876cf42b6d0446cf9
Mano Sriram <mano.sriram0@gmail.com>
2020-12-19 13:00:19 +0530

    update return message

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const app = require("express")();
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-    return res.send({ message: "Hello, there from Node Application" });
+    return res.send({
+        message: "Hello, there from Node Github actions application"
+    });
 });
 
 app.listen(PORT, () => {
